0e0b5836984c3a58c1f1fc9ca89236518e690172
Parham Alvani <parham.alvani@gmail.com>
2017-12-30 02:21:50 +0330

    Adds string method for match.

--- algorithm/algorithm.go
+++ algorithm/algorithm.go
@@ -10,10 +10,28 @@
 
 package algorithm
 
-import "github.com/1995parham/InputBuffer/switches"
+import (
+	"fmt"
+
+	"github.com/1995parham/InputBuffer/switches"
+)
 
 // Match represents input to output port matching
 type Match map[int]int
+
+func (m Match) String() string {
+	s := ""
+
+	for i, o := range m {
+		if o == -1 {
+			s += fmt.Sprintf("%d -> x\n", i)
+		} else {
+			s += fmt.Sprintf("%d -> %d\n", i, o)
+		}
+	}
+
+	return s
+}
 
 // Algorithm represents switching fabric algorithm
 type Algorithm interface {
