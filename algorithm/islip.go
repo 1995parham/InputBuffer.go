f5bc322e00bd17f64720e4a97711a4a50025b828
Parham Alvani <parham.alvani@gmail.com>
2017-12-30 02:11:11 +0330

    Adds islip and its test.

---
+++ algorithm/islip.go
@@ -0,0 +1,91 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 30-12-2017
+ * |
+ * | File Name:     islip.go
+ * +===============================================
+ */
+
+package algorithm
+
+import "github.com/1995parham/InputBuffer/switches"
+
+// ISLIP represents Iterative Round-Robin with SLIP Matching Algorithm
+type ISLIP struct {
+	AcceptArbiter []int
+	GrantArbiter  []int
+}
+
+// NewISLIP builds new instance of iSLIP for n port switch
+func NewISLIP(n int) *ISLIP {
+	return &ISLIP{
+		AcceptArbiter: make([]int, n),
+		GrantArbiter:  make([]int, n),
+	}
+}
+
+// Iterate runs iSLIP algorithm on given switch
+func (s *ISLIP) Iterate(sw *switches.Switch) Match {
+	if sw.N != len(s.AcceptArbiter) || sw.N != len(s.GrantArbiter) {
+		panic("invalid switch size")
+	}
+
+	m := make(map[int]int)
+
+	// Request phase
+	r := make(map[int][]int)
+	// For each input port
+	for i := 0; i < sw.N; i++ {
+		for j := 0; j < sw.N; j++ {
+			if sw.Ports[i].VOQ[j] != 0 {
+				r[j] = append(r[j], i)
+			}
+		}
+	}
+
+	// Grant phase
+	g := make(map[int][]int)
+	// For each output port
+	for i := 0; i < sw.N; i++ {
+		granted := false
+		for j := 0; j < sw.N; j++ {
+			for _, in := range r[i] {
+				if in == s.GrantArbiter[i] {
+					if !granted {
+						granted = true
+						g[in] = append(g[in], i)
+					}
+				}
+			}
+			if granted {
+				break
+			}
+			s.GrantArbiter[i] = (s.GrantArbiter[i] + 1) % sw.N
+		}
+	}
+
+	// Accept phase
+	// For each input port
+	for i := 0; i < sw.N; i++ {
+		accepted := false
+		for k, j := 0, s.AcceptArbiter[i]; k < sw.N; k, j = k+1, (j+1)%sw.N {
+			for _, out := range g[i] {
+				if out == j {
+					if !accepted {
+						accepted = true
+						m[i] = out
+						s.GrantArbiter[out] = (s.GrantArbiter[out] + 1) % sw.N
+						s.AcceptArbiter[i] = (j + 1) % sw.N
+					}
+				}
+			}
+		}
+		if !accepted {
+			m[i] = -1
+		}
+	}
+
+	return m
+}
---
+++ algorithm/islip_test.go
@@ -0,0 +1,70 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 30-12-2017
+ * |
+ * | File Name:     islip_test.go
+ * +===============================================
+ */
+
+package algorithm
+
+import (
+	"testing"
+
+	"github.com/1995parham/InputBuffer/switches"
+)
+
+//
+// Input 1
+//   * 1
+// * * 2
+//     3
+//     4
+//
+// Input 2
+//     1
+//     2
+//     3
+//     4
+//
+// Input 3
+//     1
+// * * 2
+//     3
+//   * 4
+//
+// Input 4
+//     1
+//     2
+//     3
+// * * 4
+func TestISLIP(t *testing.T) {
+	sw := switches.New(4)
+
+	sw.Ports[0].VOQ[0] = 1
+	sw.Ports[0].VOQ[1] = 2
+
+	sw.Ports[2].VOQ[1] = 2
+	sw.Ports[2].VOQ[3] = 1
+
+	sw.Ports[3].VOQ[3] = 2
+
+	alg := NewISLIP(4)
+
+	alg.AcceptArbiter[0] = 0
+	alg.AcceptArbiter[1] = 0
+	alg.AcceptArbiter[2] = 0
+	alg.AcceptArbiter[3] = 0
+
+	alg.GrantArbiter[0] = 0
+	alg.GrantArbiter[1] = 0
+	alg.GrantArbiter[2] = 0
+	alg.GrantArbiter[3] = 0
+
+	m := alg.Iterate(sw)
+	if m[0] != 0 || m[1] != -1 || m[2] != 3 || m[3] != -1 {
+		t.Fatalf("Matching must be [0:0 1:-1 2:3 3:-1] but it is %v\n", m)
+	}
+}
