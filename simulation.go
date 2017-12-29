8c15575f17a83b0e0f80f8d6f0d6fd600534bbbe
Parham Alvani <parham.alvani@gmail.com>
2017-12-29 23:39:41 +0330

    Adds simulation struct.

---
+++ simulation.go
@@ -0,0 +1,17 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 29-12-2017
+ * |
+ * | File Name:     simulation.go
+ * +===============================================
+ */
+
+package main
+
+import "github.com/1995parham/InputBuffer/switches"
+
+type simulation struct {
+	sw switches.Switch
+}
