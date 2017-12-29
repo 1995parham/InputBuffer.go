07bd52e08ce655a765db58001e5e876b0ed8eab3
Parham Alvani <parham.alvani@gmail.com>
2017-12-30 00:04:50 +0330

    Adds basic test for algorithm.

---
+++ algorithm/algorithm_test.go
@@ -0,0 +1,41 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 29-12-2017
+ * |
+ * | File Name:     algorithm_test.go
+ * +===============================================
+ */
+
+package algorithm
+
+import (
+	"testing"
+
+	"github.com/1995parham/InputBuffer/switches"
+)
+
+type dummy struct{}
+
+func (d *dummy) Iterate(s *switches.Switch) Match {
+	return map[int]int{
+		0: -1,
+		1: 1,
+	}
+}
+
+func TestBasic(t *testing.T) {
+	var alg Algorithm
+	alg = &dummy{}
+
+	sw := switches.New(2)
+
+	m := alg.Iterate(sw)
+
+	for i := 0; i < 2; i++ {
+		if m[i] >= 2 {
+			t.Fatalf("Input %d -> Output %d but %d >= 2\n", i, m[i])
+		}
+	}
+}
