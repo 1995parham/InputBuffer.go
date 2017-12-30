36e18cfa074deef2c2beafdb0e87d779d5cc1c1b
Parham Alvani <parham.alvani@gmail.com>
2017-12-30 22:27:03 +0330

    Adds islip simulation.

---
+++ simulation/islip/main.go
@@ -0,0 +1,53 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 30-12-2017
+ * |
+ * | File Name:     main.go
+ * +===============================================
+ */
+
+package main
+
+import (
+	"github.com/1995parham/InputBuffer/algorithm"
+	"github.com/1995parham/InputBuffer/simulation"
+	"github.com/1995parham/InputBuffer/switches"
+)
+
+func main() {
+	sw := switches.New(4)
+
+	sw.Ports[0].VOQ[0] = 3
+	sw.Ports[0].VOQ[1] = 1
+	sw.Ports[0].VOQ[2] = 2
+
+	sw.Ports[1].VOQ[0] = 1
+	sw.Ports[1].VOQ[2] = 2
+	sw.Ports[1].VOQ[3] = 1
+
+	sw.Ports[2].VOQ[1] = 1
+	sw.Ports[2].VOQ[2] = 3
+	sw.Ports[2].VOQ[3] = 1
+
+	sw.Ports[3].VOQ[1] = 1
+	sw.Ports[3].VOQ[2] = 1
+	sw.Ports[3].VOQ[3] = 1
+
+	alg := algorithm.NewISLIP(4)
+
+	alg.AcceptArbiter[0] = 3
+	alg.AcceptArbiter[1] = 2
+	alg.AcceptArbiter[2] = 2
+	alg.AcceptArbiter[3] = 0
+
+	alg.GrantArbiter[0] = 1
+	alg.GrantArbiter[1] = 3
+	alg.GrantArbiter[2] = 2
+	alg.GrantArbiter[3] = 0
+
+	s := simulation.New(sw, alg)
+
+	s.Simulate(3)
+}
