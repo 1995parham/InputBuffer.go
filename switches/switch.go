d534143381638a0d71e0bb8ea25b795a89e04fd3
Parham Alvani <parham.alvani@gmail.com>
2017-12-30 21:46:13 +0330

    Adds string method for switch.

--- switches/switch.go
+++ switches/switch.go
@@ -10,6 +10,8 @@
 
 package switches
 
+import "fmt"
+
 // Port represents input port of switch
 type Port struct {
 	VOQ []int // Virtual Output Queue
@@ -20,6 +22,26 @@ type Port struct {
 type Switch struct {
 	Ports []*Port
 	N     int
+}
+
+func (sw *Switch) String() string {
+	s := ""
+
+	s += fmt.Sprintf("==============\n")
+	for i := 0; i < sw.N; i++ {
+		s += fmt.Sprintf("Input %d:\n", i)
+		for j := 0; j < sw.N; j++ {
+			s += fmt.Sprintf("%*s\n", sw.Ports[i].VOQ[j], "---")
+			for k := 0; k < sw.Ports[i].VOQ[j]; k++ {
+				s += fmt.Sprintf("*")
+			}
+			s += fmt.Sprintf("|\n")
+			s += fmt.Sprintf("%*s\n", sw.Ports[i].VOQ[j], "---")
+		}
+	}
+	s += fmt.Sprintf("==============\n")
+
+	return s
 }
 
 // New creates new switch with n in/out ports
