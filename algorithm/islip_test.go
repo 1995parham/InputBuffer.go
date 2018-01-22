0a1a0e2da8d0b2125b0d8ab786225c170a1dc8c1
Parham Alvani <parham.alvani@gmail.com>
2018-01-23 02:16:13 +0330

    Adds test case for islip-2.

--- algorithm/islip_test.go
+++ algorithm/islip_test.go
@@ -51,6 +51,7 @@ func TestISLIP(t *testing.T) {
 
 	sw.Ports[3].VOQ[3] = 2
 
+	// iSLIP-1
 	alg := NewISLIP(4, 1)
 
 	alg.AcceptArbiter[0] = 0
@@ -64,6 +65,24 @@ func TestISLIP(t *testing.T) {
 	alg.GrantArbiter[3] = 0
 
 	m := alg.Iterate(sw)
+	if m[0] != 0 || m[1] != -1 || m[2] != 3 || m[3] != -1 {
+		t.Fatalf("Matching must be [0:0 1:-1 2:3 3:-1] but it is %v\n", m)
+	}
+
+	// iSLIP-2
+	alg = NewISLIP(4, 2)
+
+	alg.AcceptArbiter[0] = 0
+	alg.AcceptArbiter[1] = 0
+	alg.AcceptArbiter[2] = 0
+	alg.AcceptArbiter[3] = 0
+
+	alg.GrantArbiter[0] = 0
+	alg.GrantArbiter[1] = 0
+	alg.GrantArbiter[2] = 0
+	alg.GrantArbiter[3] = 0
+
+	m = alg.Iterate(sw)
 	if m[0] != 0 || m[1] != -1 || m[2] != 3 || m[3] != -1 {
 		t.Fatalf("Matching must be [0:0 1:-1 2:3 3:-1] but it is %v\n", m)
 	}
