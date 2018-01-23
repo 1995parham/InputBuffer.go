e4ec7b0616bf193ac4bf5055b429cd4bcf0d479f
Parham Alvani <parham.alvani@gmail.com>
2018-01-23 12:03:01 +0330

    Let's do it in Go.

---
+++ algorithm/rr_lqf.go
@@ -0,0 +1,11 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 23-01-2018
+ * |
+ * | File Name:     rr_lqf.go
+ * +===============================================
+ */
+
+package algorithm
