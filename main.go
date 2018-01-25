bb349a8c740ad0b5ee186bf247128ba01d7e7581
Parham Alvani <parham.alvani@gmail.com>
2018-01-26 00:47:41 +0330

    Adds main.

---
+++ main.go
@@ -0,0 +1,37 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 26-01-2018
+ * |
+ * | File Name:     main.go
+ * +===============================================
+ */
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/AUTProjects/InputBuffer/config"
+)
+
+func main() {
+	var f string
+
+	fmt.Print("Simulation file name: ")
+	fmt.Scanf("%s", &f)
+
+	data, err := ioutil.ReadFile(f)
+	if err != nil {
+		log.Fatalf("Reading file %s failed with: %s\n", f, err)
+		os.Exit(1)
+	}
+
+	if err := config.Parse(data); err != nil {
+		log.Fatalf("Parsing file %s failed with: %s\n", f, err)
+	}
+}
