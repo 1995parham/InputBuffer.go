b1ae3e9bd912e698a3a06202623391b97b57f07a
Parham Alvani <parham.alvani@gmail.com>
2018-01-25 23:47:18 +0330

    Adds remote test.

---
+++ algorithm/remote_test.go
@@ -0,0 +1,56 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 25-01-2018
+ * |
+ * | File Name:     algorithm/remote_test.go
+ * +===============================================
+ */
+
+package algorithm
+
+import (
+	"net/http/httptest"
+	"net/rpc"
+	"testing"
+
+	"github.com/AUTProjects/InputBuffer/switches"
+	"github.com/powerman/rpc-codec/jsonrpc2"
+)
+
+type null struct{}
+
+func (null) Iterate(sw switches.Switch, m *Match) error {
+	*m = make(map[int]int)
+
+	for i := 0; i < sw.N; i++ {
+		(*m)[i] = i
+	}
+
+	return nil
+}
+
+func TestRemote(t *testing.T) {
+	rpc := rpc.NewServer()
+	rpc.RegisterName("Algorithm", null{})
+
+	h := jsonrpc2.HTTPHandler(rpc)
+	s := httptest.NewServer(h)
+	defer s.Close()
+
+	sw := switches.New(8)
+
+	r := NewRemote(s.URL)
+
+	m := r.Iterate(sw)
+
+	if len(m) != sw.N {
+		t.Fatalf("Matching size is %d instead of %d", len(m), sw.N)
+	}
+	for i := range m {
+		if m[i] != i {
+			t.Fatalf("m[%d] -> %d != %d", i, m[i], i)
+		}
+	}
+}
