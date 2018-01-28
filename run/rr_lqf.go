f4fca355d3c4ef0ab2799b2e408eeb14a1eef3c8
Parham Alvani <parham.alvani@gmail.com>
2018-01-28 22:54:41 +0330

    Adds rrlqf algorithm parser.

---
+++ run/rr_lqf.go
@@ -0,0 +1,25 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 28-01-2018
+ * |
+ * | File Name:     run/rr_lqf.go
+ * +===============================================
+ */
+
+package run
+
+import "github.com/AUTProjects/InputBuffer.go/algorithm"
+
+func init() {
+	m["RR/LQF"] = parseRRLQF
+}
+
+func parseRRLQF(parameters map[interface{}]interface{}, n int) (algorithm.Algorithm, error) {
+	i := parameters["iterations"].(int)
+
+	alg := algorithm.NewRRLQF(n, i)
+
+	return alg, nil
+}
