025fc527d8394f45caa1e4c8bf163bcc13f41134
Parham Alvani <parham.alvani@gmail.com>
2018-01-28 21:03:31 +0330

    Better simulator :dancer:.

--- simulation/simulation.go
+++ simulation/simulation.go
@@ -12,6 +12,7 @@ package simulation
 
 import (
 	"fmt"
+	"math/rand"
 
 	"github.com/AUTProjects/InputBuffer.go/algorithm"
 	"github.com/AUTProjects/InputBuffer.go/switches"
@@ -22,25 +23,36 @@ import (
 type Simulation struct {
 	sw  *switches.Switch
 	alg algorithm.Algorithm
+	p   int
 }
 
 // New creates new simulation instance
-func New(sw *switches.Switch, alg algorithm.Algorithm) *Simulation {
+func New(sw *switches.Switch, alg algorithm.Algorithm, p int) *Simulation {
 	return &Simulation{
 		sw:  sw,
 		alg: alg,
+		p:   p,
 	}
 }
 
 // Simulate run simulation by given number of time slots
 func (s *Simulation) Simulate(timeslots int) {
 	counter := 0
+	throughput := 0.0
+
 	for counter < timeslots {
 		fmt.Printf("%20s%d\n", "T=", counter)
 		fmt.Println(s.sw)
 
 		// TODO: Let's generate traffic
-		fmt.Println("Generate random traffic")
+		fmt.Println("Generate uniform traffic")
+		in := 0
+		for i := 0; i < s.sw.N; i++ {
+			if rand.Intn(100) < s.p {
+				in++
+				s.sw.Ports[i].VOQ[rand.Intn(s.sw.N)]++
+			}
+		}
 
 		m := s.alg.Iterate(s.sw)
 
@@ -48,12 +60,18 @@ func (s *Simulation) Simulate(timeslots int) {
 		fmt.Println(m)
 		// TODO: Adds print information about algorithm
 
+		out := 0
 		for i, o := range m {
 			if o != -1 && s.sw.Ports[i].VOQ[o] > 0 {
 				s.sw.Ports[i].VOQ[o]--
+				out++
 			}
 		}
 
-		timeslots--
+		counter++
+		throughput += float64(out) / float64(in)
+		fmt.Printf("In: %d, Out: %d, Throughput: %g\n", in, out, float64(out)/float64(in))
 	}
+
+	fmt.Printf("Average Throughput: %g\n", throughput/float64(timeslots)*100)
 }
