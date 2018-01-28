b1a4f7028022eedb26aa588b15a7d8dad457dcbe
Parham Alvani <parham.alvani@gmail.com>
2018-01-28 22:01:46 +0330

    Tests new switch features.

--- switches/switch_test.go
+++ switches/switch_test.go
@@ -20,8 +20,36 @@ func TestBasic(t *testing.T) {
 	}
 
 	for _, p := range sw.Ports {
-		if len(p.VOQ) != 2 {
-			t.Fatalf("Switch input-port VOQ length must be equal to 2 not %d\n", len(p.VOQ))
+		if len(p.voq) != 2 {
+			t.Fatalf("Switch input-port VOQ length must be equal to 2 not %d\n", len(p.voq))
 		}
+	}
+}
+
+func TestMatching(t *testing.T) {
+	sw := New(2)
+
+	sw.Arrive(0, 0)
+	sw.Arrive(0, 1)
+	sw.Arrive(1, 1)
+
+	sw.Process(map[int]int{
+		0: 0,
+		1: 1,
+	})
+
+	ps := sw.Process(map[int]int{
+		0: 1,
+		1: 0,
+	})
+
+	if len(ps) != 1 {
+		t.Fatalf("There must be one packet but there are/is %d packet[s]", len(ps))
+	}
+	if sw.t != 2 {
+		t.Fatalf("Two timeslots passed from starting not %d", sw.t)
+	}
+	if ps[0].InputPort != 0 && ps[0].OutputPort != 1 && ps[0].Delay != 1 {
+		t.Fatalf("0 -> 1 aftter 1 timeslots != %s", ps[0])
 	}
 }
