131eb983963241b78c4521be44aa4c2af27adf16
Parham Alvani <parham.alvani@gmail.com>
2018-01-29 13:35:36 +0330

    Parse and create status writer.

--- main.go
+++ main.go
@@ -12,12 +12,19 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 
 	"github.com/AUTProjects/InputBuffer.go/run"
 )
+
+type fakeWriter struct{}
+
+func (*fakeWriter) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
 
 func main() {
 	var f string
@@ -26,10 +33,16 @@ func main() {
 	fmt.Print("Simulation file name: ")
 	fmt.Scanf("%s", &f)
 
+	var w io.Writer = &fakeWriter{}
+
 	fmt.Print("Results file name (empty if you do not want it): ")
 	fmt.Scanf("%s", &r)
 	if r != "" {
 		r = r + ".results"
+		f, err := os.OpenFile(r, os.O_WRONLY|os.O_CREATE, 0755)
+		if err == nil {
+			w = f
+		}
 	}
 
 	data, err := ioutil.ReadFile(f)
@@ -38,7 +51,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := run.Run(data); err != nil {
+	if err := run.Run(data, w); err != nil {
 		log.Fatalf("Parsing file %s failed with: %s\n", f, err)
 	}
 }
