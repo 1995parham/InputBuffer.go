676f79cb23419ab725e5c8a7c49257f777c61125
Parham Alvani <parham.alvani@gmail.com>
2018-01-29 23:11:46 +0330

    Adds runner for remote algorithm.

---
+++ run/remote.go
@@ -0,0 +1,25 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 29-01-2018
+ * |
+ * | File Name:     run/remote.go
+ * +===============================================
+ */
+
+package run
+
+import "github.com/AUTProjects/InputBuffer.go/algorithm"
+
+func init() {
+	m["Remote"] = parseRemote
+}
+
+func parseRemote(parameters map[interface{}]interface{}, n int) (algorithm.Algorithm, error) {
+	u := parameters["url"].(string)
+
+	alg := algorithm.NewRemote(u)
+
+	return alg, nil
+}
